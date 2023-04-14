29183a78c049a31f6bfd527a32bda3934eec097c
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-14 15:45:40 +0300

    adding buttom with onclick function

--- frontend/src/Components/RegisterForm/RegisterForm.jsx
+++ frontend/src/Components/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,11 @@
 import React, {useState}  from 'react'
 import InputForm from '../InputForm/InputForm'
+import ButtonComponent from '../Button/ButtonComponent';
 const Register = () => {
     const [username,setUserName]=useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()}
   return (
     <div className='wrapper'>
         <div className='sign-in-up-container'>
@@ -18,7 +22,7 @@ const Register = () => {
                 label="username"
                 name="username"
                 value={username}/>
-            
+            <ButtonComponent color="yellow"size="15px" onClick={handleSubmit}>Register</ButtonComponent> 
         </div>
       
     </div>
