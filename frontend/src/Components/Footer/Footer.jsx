f1de3e3397beafa16763dc49385421de289f8aa9
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-14 11:52:45 +0300

    finalizing css for the footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
   return (
     <div className="footer">
     <div className="info">
-    <a href="https://CodeJunction.com/mobile-app"><p className='info-details'>Mobile app</p></a>
-    <a href="https://CodeJunction/community"><p className='info-details'>Community</p></a>
-    <a href="https://CodeJunction/company"><p className='info-details'>Company</p></a>
-    <a href="https://CodeJunction/help-desk"><p className='info-details'>Help desk</p></a>
-    <a href="https://CodeJunction/blog"><p className='info-details'>Blog</p></a>
-    <a href="https://CodeJunction/resources"><p className='info-details'>Resources</p></a>
+    <a href="https://CodeJunction.com/mobile-app"><p className='footer-details'>Mobile app</p></a>
+    <a href="https://CodeJunction/community"><p className='footer-details'>Community</p></a>
+    <a href="https://CodeJunction/company"><p className='footer-details'>Company</p></a>
+    <a href="https://CodeJunction/help-desk"><p className='footer-details'>Help desk</p></a>
+    <a href="https://CodeJunction/blog"><p className='footer-details'>Blog</p></a>
+    <a href="https://CodeJunction/resources"><p className='footer-details'>Resources</p></a>
     </div>
     <div className="connect">
         <div className="icons-container">
