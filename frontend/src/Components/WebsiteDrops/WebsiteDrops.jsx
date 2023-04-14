97ae3ea275ca6fd006afabcddc7673da73a18254
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-14 19:08:17 +0300

    adding more div (drops)

--- frontend/src/Components/WebsiteDrops/WebsiteDrops.jsx
+++ frontend/src/Components/WebsiteDrops/WebsiteDrops.jsx
@@ -2,12 +2,30 @@ import React from 'react'
 
 const WebsiteDrops = () => {
   return (
-    <div className="wrapper">
+     <div className="wrapper">
     <div className="container">
       <div className="drop">
         <div className="content">
           <h2>01</h2>
           <p>Collaborate and improve code in real-time with live sharing and pair programming.</p>
+        </div>
+      </div>
+      <div className="drop">
+        <div className="content">
+          <h2>02</h2>
+          <p>Automatically analyze code quality, performance, and security with integrated tools.</p>
+        </div>
+      </div>
+      <div className="drop">
+        <div className="content">
+          <h2>03</h2>
+          <p>Add fun and motivation to coding with gamification elements like badges and leaderboards.</p>
+        </div>
+      </div>
+      <div className="drop">
+        <div className="content">
+          <h2>04</h2>
+          <p>Enhance your coding skills and learn from other developers with our code review platform.</p>
         </div>
       </div>
     </div>
