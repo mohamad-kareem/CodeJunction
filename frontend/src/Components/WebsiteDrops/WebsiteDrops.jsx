9627429a659e274af1535e6a06cba8ec4ca85072
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-15 01:41:49 +0300

    adding the other divs with component

--- frontend/src/Components/WebsiteDrops/WebsiteDrops.jsx
+++ frontend/src/Components/WebsiteDrops/WebsiteDrops.jsx
@@ -14,6 +14,9 @@ const WebsiteDrops = () => {
   return (
      <motion.div className="drop-wrapper " variants={containerVariants}  animate="animate">
    <InnerDrop  Heading="01" content="Collaborate and improve code in real-time with live sharing and pair programming."/>
+   <InnerDrop  Heading="02" content="Automatically analyze code quality, performance, and security with integrated tools."/>
+   <InnerDrop  Heading="03" content="Add fun and motivation to coding with gamification elements leaderboards."/>
+   <InnerDrop  Heading="04" content="Enhance your coding skills and learn from other developers with our code review platform."/>
      </motion.div>
   )
 }
