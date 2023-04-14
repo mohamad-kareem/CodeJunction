cc1da6a332d8ba42560af53c0044aaefb4409636
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-14 09:16:04 +0300

    creating navbar component

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./navbar.css"
+const Navbar = () => {
+
+
+  return (
+    <div className="navbar">
+			<div className="logo">
+				<span className='partLogo'>Code</span>Junction
+			</div>
+			<div className="links">
+				<Link to="/home" className="linking">
+					Home
+				</Link>
+				<Link to="/editor" className="linking">
+					Code Editor
+				</Link>
+                <Link to="/codes" className="linking">
+					Codes
+				</Link>
+                <Link to="/dashboard" className="linking">
+					DashBoard
+				</Link>
+			
+                <ButtonComponent id="logout" className="linking" width='80px'> LogOut</ButtonComponent>
+			</div>
+		</div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default LandingPage
