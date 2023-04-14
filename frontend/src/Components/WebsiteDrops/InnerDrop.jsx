c7d2c356c9c13b512102e9c531b0ad2e02d4b96b
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-15 01:57:10 +0300

    adding motion for the component and move every thing related to motion to the inner drop component

--- frontend/src/Components/WebsiteDrops/InnerDrop.jsx
+++ frontend/src/Components/WebsiteDrops/InnerDrop.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
 const InnerDrop = ({Heading,content}) => {
+
+    const containerVariants = {
+        animate: {
+          x: [-10, 0, -10],
+          transition: { duration: 1.5, repeat: Infinity },
+        },
+      };
+    
+
+    const dropVariants = {
+        animate: {
+          y: [-10, 0, -10],
+          transition: { duration: 1.5, repeat: Infinity, delay: 0.2 },
+        },
+      };
+    
+      const contentVariants = {
+        animate: {
+          y: [-10, 0, -10],
+          transition: { duration: 1.5, repeat: Infinity, delay: 0.4 },
+        },
+      };
+
   return (
-   <div className="container">
-     <div className="drop">
-       <div className="content">
+   <motion.div className="container" variants={containerVariants} animate="animate">
+     <motion.div className="drop"variants={dropVariants}  animate="animate">
+       <motion.div className="content" variants={contentVariants} animate="animate">
          <h2>{Heading}</h2>
          <p>{content}</p>
-       </div>
-     </div>
-   </div>
+       </motion.div>
+     </motion.div>
+   </motion.div>
   )
 }
 
--- frontend/src/Components/WebsiteDrops/WebsiteDrops.jsx
+++ frontend/src/Components/WebsiteDrops/WebsiteDrops.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import "./websitedrops.css"
-import { motion } from 'framer-motion';
 import InnerDrop from './InnerDrop';
 const WebsiteDrops = () => {
 
-  const containerVariants = {
-    animate: {
-      x: [-10, 0, -10],
-      transition: { duration: 1.5, repeat: Infinity },
-    },
-  };
+ 
 
   return (
-     <motion.div className="drop-wrapper " variants={containerVariants}  animate="animate">
+     <div className="drop-wrapper ">
    <InnerDrop  Heading="01" content="Collaborate and improve code in real-time with live sharing and pair programming."/>
    <InnerDrop  Heading="02" content="Automatically analyze code quality, performance, and security with integrated tools."/>
    <InnerDrop  Heading="03" content="Add fun and motivation to coding with gamification elements leaderboards."/>
    <InnerDrop  Heading="04" content="Enhance your coding skills and learn from other developers with our code review platform."/>
-     </motion.div>
+     </div>
   )
 }
 
