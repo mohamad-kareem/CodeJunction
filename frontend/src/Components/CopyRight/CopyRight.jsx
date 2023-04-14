3ff25ed1c3a5dae2baad852682e9103ea47d7665
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-14 10:30:28 +0300

    creating copyright component

---
+++ frontend/src/Components/CopyRight/CopyRight.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./copyright.css"
+const CopyRights = ({marginTop}) => {
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+<div className="copyrights" style={{marginTop}}>
+      <p>© {currentYear} CodeJunction B.V. All rights reserved.</p>
+    </div>  )
+}
+
+export default CopyRights
