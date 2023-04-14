e1f88c34bc64b57f924e0e5500262d930683238f
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-14 09:46:51 +0300

    adding styling for the landing intro

--- frontend/src/Components/LandingIntro/LandingIntro.jsx
+++ frontend/src/Components/LandingIntro/LandingIntro.jsx
@@ -11,7 +11,7 @@ const LandingIntro = () => {
     <div className='intro-container'>
     <div className='intro-content'>
             <h1>Unlock your coding<br/>  potential with collaboration </h1>
-             <div className='btn-intro'> <ButtonComponent width='100px'size={"10px"} onClick={handleClick}>Get Started</ButtonComponent></div>
+             <div className='btn-intro'> <ButtonComponent width='130px'size={"15px"} onClick={handleClick}>Get Started</ButtonComponent></div>
         </div>
     </div>
 
