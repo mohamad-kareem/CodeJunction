7fb10dbb14630307301140348059dead58b540d8
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-15 04:46:04 +0300

    adding more sections on the side bar

--- frontend/src/Components/SideBar/SideBarContent.jsx
+++ frontend/src/Components/SideBar/SideBarContent.jsx
@@ -1,9 +1,26 @@
-import React from "react"
 import HomeIcon from '@mui/icons-material/Home';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import SourceIcon from '@mui/icons-material/Source';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export const SideBarContent=[
     {
         title:"Home",
         icon:<HomeIcon/>,
         link:"/home"
+    },
+    {
+        title:"Ranking",
+        icon:<EmojiEventsIcon/>,
+        link:"/home"
+    },
+    {
+        title:"Code",
+        icon:<SourceIcon/>,
+        link:"/home"
+    },
+    {
+        title:"Github",
+        icon:<GitHubIcon/>,
+        link:"/"
     },]
