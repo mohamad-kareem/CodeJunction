0aa9e95e3cbbda0465f5cbba23423aea2fc49eb7
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-15 06:07:38 +0300

    adding widgets and styling them

---
+++ frontend/src/Components/Widget/Widget.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./widget.css"
+
+const Widget = () => {
+  return (
+    <div className='widget'>
+        <div className='left'>left</div>
+        <div className='right'>right</div>
+      
+    </div>
+  )
+}
+
+export default Widget
--- frontend/src/Pages/DashBoardPage/DashBoard.jsx
+++ frontend/src/Pages/DashBoardPage/DashBoard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import SideBar from '../../Components/SideBar/SideBar'
 import "./dashboard.css"
+import Widget from '../../Components/Widget/Widget'
+import Navbar from '../../Components/Navbar/Navbar'
 const DashBoard = () => {
   return (
-    <div className='sidebar-wrapper'>
-      <SideBar/>
+    
+    <div className='dashboard-wrapper'>
+   
+       <div className='sidebar-wrapper'>
+          <SideBar/>
+       </div>
+       <div className='widget-container'>
+        <Widget/>
+        <Widget/>
+        <Widget/>
+
+       </div>
     </div>
+   
   )
 }
 
