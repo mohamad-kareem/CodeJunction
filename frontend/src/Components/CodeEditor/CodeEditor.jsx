34c72683800e8ec751cee956a4ada9974989caa4
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-17 01:45:01 +0300

    disable mounting twice in the code editor page

--- frontend/src/Components/CodeEditor/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -8,18 +8,19 @@ import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
 
 const CodeEditor = () => {
-  useEffect(()=>{
-    async function init(){
-       Codemirror.fromTextArea(document.getElementById('codeEditorCollaboration'),{
-        mode:{name:'javascript',json:true},
-        theme:'dracula',
-        autoCloseTags:true,
-        autoCloseBrackets:true,
-        lineNumbers:true,
-       });
-    }
-    init();
-  },[]);
+  useEffect(() => {
+    const editor = Codemirror.fromTextArea(document.getElementById('codeEditorCollaboration'), {
+      mode: { name: 'javascript', json: true },
+      theme: 'dracula',
+      autoCloseTags: true,
+      autoCloseBrackets: true,
+      lineNumbers: true,
+    });
+  
+    return () => {
+      editor.toTextArea();
+    };
+  }, []);
   return (
     <textarea id='codeEditorCollaboration'></textarea>
   )
