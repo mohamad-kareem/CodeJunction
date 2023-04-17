0bbcf81b9b19c10978bd7e7b7e710f8fce3ce974
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-17 13:22:45 +0300

    set username in input form at session page

--- frontend/src/Pages/SessionForm/SessionForm.jsx
+++ frontend/src/Pages/SessionForm/SessionForm.jsx
@@ -7,7 +7,8 @@ import CopyRights from '../../Components/CopyRight/CopyRight'
 import { faUserSecret,faKey} from '@fortawesome/free-solid-svg-icons';
 import {v4 as uuidV4} from "uuid";
 const SessionForm = () => {
-    const [roomId,setRoomId]=useState("")
+    const [roomId,setRoomId]=useState("");
+    const [username,setUserName]=useState("")
     const generateNewId=(e)=>{
         e.preventDefault();
         const id=uuidV4()
@@ -23,13 +24,16 @@ const SessionForm = () => {
                     label="username"
                     name="username"
                     icon={faUserSecret}
-                    value={roomId}
-                    onChange={(e)=>setRoomId(e.target.value)}
+                    value={username}
+                    onChange={(e)=>setUserName(e.target.value)}
+                   
                 />
                 <InputForm
                     label="ID ROOM"
                     name="ID ROOM"
                     icon={faKey}
+                    value={roomId}
+                    onChange={(e)=>setRoomId(e.target.value)}
                 />
                 <ButtonComponent children={"Join"} width='100px' className="join-btn"/>
                 <span className='generate-container'>Generate room ID  <a href="" className='generate' onClick={generateNewId}>Click Here</a> </span>
