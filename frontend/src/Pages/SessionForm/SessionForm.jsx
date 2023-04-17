c0975f8e410431075b25f2ac60a2ee2570baeb94
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-17 13:08:58 +0300

    generating random room id

--- frontend/src/Pages/SessionForm/SessionForm.jsx
+++ frontend/src/Pages/SessionForm/SessionForm.jsx
@@ -3,25 +3,33 @@ import InputForm from '../../Components/InputForm/InputForm'
 import ButtonComponent from '../../Components/Button/ButtonComponent'
 import "./sessionform.css"
 import Navbar from '../../Components/Navbar/Navbar'
-import Logo from '../../Components/Logo/Logo'
 import CopyRights from '../../Components/CopyRight/CopyRight'
+import { faUserSecret,faKey} from '@fortawesome/free-solid-svg-icons';
+import {v4 as uuidV4} from "uuid";
 const SessionForm = () => {
+    const generateNewId=(e)=>{
+        e.preventDefault();
+        const id=uuidV4()
+        console.log(id)
+    }
   return (
     <div className="page-wrapper">
             <Navbar/>
         <div className='session-wrapper'>
             <div className="inner-form">
             <h1>War Room</h1>
-                <InputForm
-                label="ID ROOM"
-                name="ID ROOM"
+              <InputForm
+                    label="username"
+                    name="username"
+                    icon={faUserSecret}
                 />
                 <InputForm
-                label="username"
-                name="username"
+                    label="ID ROOM"
+                    name="ID ROOM"
+                    icon={faKey}
                 />
                 <ButtonComponent children={"Join"} width='100px' className="join-btn"/>
-                <span className='generate-container'>Generate room ID &nbsp; <a href="" className='generate'>Click Here</a> </span>
+                <span className='generate-container'>Generate room ID  <a href="" className='generate' onClick={generateNewId}>Click Here</a> </span>
             </div>
         </div>
         <CopyRights/>
