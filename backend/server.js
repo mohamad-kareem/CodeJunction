4e41184503551f42a146115e7bc0f061ebacc0de
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-18 11:57:41 +0300

    connecting socket

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
+const { Socket } = require('dgram');
 const express =require('express');
 const app= express();
 const http = require('http');
 const server= http.createServer(app);
+
+const {Server}=require('socket.io');
+const io=new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('socket connected' , socket.io);
+});
+
+const Port=5000;
+server.listen(Port, ()=>console.log(`listening on Port ${Port}`));
