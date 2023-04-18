8ffda9bf2a28ba2b661f6980be3232b91bc9b4a8
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-18 13:08:35 +0300

    importing do list and joining via socket

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
-const { Socket } = require('dgram');
 const express =require('express');
 const app= express();
 const http = require('http');
 const server= http.createServer(app);
 
 const {Server}=require('socket.io');
+const DoList = require('../frontend/src/SocketConnections/DoList');
 const io=new Server(server);
+
+const userSocketMap={};
+
+
 
 io.on('connection', (socket) => {
     console.log('socket connected' , socket.io);
+    socket.on(DoList.JOIN , ({roomId,username})=>{
+        userSocketMap[socket.id]=username;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId)
+    })
 });
 
 const Port=8000;
