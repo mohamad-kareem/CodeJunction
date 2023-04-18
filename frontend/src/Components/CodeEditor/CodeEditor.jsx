bd71aca962cc4a1fceae160acf2bf6df090c7ceb
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-18 22:30:29 +0300

    checking the changing while typing in code editor textarea

--- frontend/src/Components/CodeEditor/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useRef } from 'react';
 import "./codeeditor.css"
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
@@ -8,14 +8,18 @@ import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
 
 const CodeEditor = () => {
+  const editorRef=useRef(null);
   useEffect(() => {
-     Codemirror.fromTextArea(document.getElementById('codeEditorCollaboration'), {
+     editorRef.current=Codemirror.fromTextArea(document.getElementById('codeEditorCollaboration'), {
       mode: { name: 'javascript', json: true },
       theme: 'elegant',
       autoCloseTags: true,
       autoCloseBrackets: true,
       lineNumbers: true,
     });
+    editorRef.current.on("change",(instance,changes)=>{
+      console.log(changes)
+    })
   
   }, []);
   return (
