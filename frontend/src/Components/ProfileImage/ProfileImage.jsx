a11f84a48652ae006612d7e8f0316b1a790fd300
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-19 17:54:10 +0300

    adding a function to convert the image to base64

--- frontend/src/Components/ProfileImage/ProfileImage.jsx
+++ frontend/src/Components/ProfileImage/ProfileImage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 const ProfileImage = () => {
+  
+    
+
+    function convertToBase64 (e){
+        var reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload= () =>{
+            console.log(reader.result);
+        }
+    }
   return (
     <div className='image-wrapper'>
         <div className="inner-image-container">
             <input 
             accept='image/*'
             type='file'
+            onChange={convertToBase64}
              />
         </div>
       
