8584bad872dac394f3349f11105e60cfaf17d01d
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-19 13:52:23 +0300

    changing the display of ranking in the dashboard

--- frontend/src/Pages/DashBoardPage/DashBoard.jsx
+++ frontend/src/Pages/DashBoardPage/DashBoard.jsx
@@ -1,10 +1,6 @@
 import React,{useState} from 'react'
 import SideBar from '../../Components/SideBar/SideBar'
 import "./dashboard.css"
-import Widget from '../../Components/Widget/Widget'
-import PersonIcon from '@mui/icons-material/Person';
-import ScoreboardIcon from '@mui/icons-material/Scoreboard';
-import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 import RankingTable from '../../Components/RankingTable/RankingTable';
 import CodesTable from '../../Components/CodesTable/CodesTable';
 import UserProfile from '../../Components/UserProfile/UserProfile';
@@ -19,27 +15,7 @@ const DashBoard = () => {
             <SideBar setCode={setCode} setRanking={setRanking} setProfile={setProfile}/>
        </div>
        <div className='middle-container'>
-         <div className="widgets-container">
-            <Widget
-                title="Users"
-                activeUsers={1234}
-                widgetLink="Active coders"
-                percentage="75%"
-                icon={PersonIcon}
-              />
-              <Widget
-                title="Points"
-                activeUsers={155}
-                widgetLink="Boost your skills"
-                icon={ScoreboardIcon}
-              />
-              <Widget
-                title="Badges"
-                activeUsers="Gold"
-                widgetLink="Boost your skills"
-                icon={MilitaryTechIcon}
-                />
-         </div>    
+         
          <div className="table">
            <RankingTable ranking={ranking}/>
            <CodesTable code={code}/>
