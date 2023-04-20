11ca4c5440cb868b7ff04856bc390122a93623a2
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-20 16:07:27 +0300

    seperating socket configuration from the main server

--- backend/server.js
+++ backend/server.js
@@ -3,62 +3,12 @@ const app= express();
 const http = require('http');
 const server= http.createServer(app);
 
-const {Server}=require('socket.io');
-const DoList = require('../frontend/src/SocketConnections/DoList');
-const io=new Server(server);
-
-const userSocketMap={};
-
-function getAllConnectedClients(roomId){
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-      (socketId)=>{
-        return {
-          socketId,
-          username:userSocketMap[socketId],
-        };
-      }
-    );
-  
-  }
-
-
-io.on('connection', (socket) => {
-    console.log('socket connected' , socket.id);
-    socket.on(DoList.JOIN , ({roomId,username})=>{
-        userSocketMap[socket.id]=username;
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-
-        clients.forEach(({socketId})=>{
-            io.to(socketId).emit(DoList.JOINED,{
-              clients,
-              username,
-              socketId:socket.id,
-            });
-      
-           });
-
-    })
-
-   socket.on(DoList.CODE_CHANGE,({roomId,code})=>{
-    socket.in(roomId).emit(DoList.CODE_CHANGE,{code});
-   });
-
-
-
-
-    socket.on("disconnecting", ()=>{
-      const rooms =[...socket.rooms];
-      rooms.forEach((roomId)=>{
-        socket.in(roomId).emit(DoList.DISCONNECTED,{
-          socketId:socket.id,
-          username:userSocketMap[socket.id],
-        });
-      })
-      delete userSocketMap[socket.id];
-      socket.leave();
-    })
-});
+const { socketServer } = require('./socketconfig');
 
 const Port=8000;
 server.listen(Port, ()=>console.log(`listening on Port ${Port}`));
+
+app.get('/hello', (req, res) => {
+  res.send('hello world');
+});
+socketServer(server);
---
+++ backend/socketconfig.js
@@ -0,0 +1,54 @@
+const { Server } = require('socket.io');
+const DoList = require('../frontend/src/SocketConnections/DoList');
+
+const userSocketMap = {};
+
+function getAllConnectedClients(io, roomId) {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
+}
+
+function socketServer(server) {
+  const io = new Server(server);
+
+  io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+    socket.on(DoList.JOIN, ({ roomId, username }) => {
+      userSocketMap[socket.id] = username;
+      socket.join(roomId);
+      const clients = getAllConnectedClients(io, roomId);
+
+      clients.forEach(({ socketId }) => {
+        io.to(socketId).emit(DoList.JOINED, {
+          clients,
+          username,
+          socketId: socket.id,
+        });
+      });
+    });
+
+    socket.on(DoList.CODE_CHANGE, ({ roomId, code }) => {
+      socket.in(roomId).emit(DoList.CODE_CHANGE, { code });
+    });
+
+    socket.on('disconnecting', () => {
+      const rooms = [...socket.rooms];
+      rooms.forEach((roomId) => {
+        socket.in(roomId).emit(DoList.DISCONNECTED, {
+          socketId: socket.id,
+          username: userSocketMap[socket.id],
+        });
+      });
+      delete userSocketMap[socket.id];
+      socket.leave();
+    });
+  });
+}
+
+module.exports = { socketServer };
