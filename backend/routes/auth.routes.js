46cbffceeb874d53127fc49047cacca29bd9c769
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-21 03:32:52 +0300

    adding routes and secret key

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 
-const {  register } = require("../controllers/auth.controller")
+const { login, register } = require("../controllers/auth.controller")
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
