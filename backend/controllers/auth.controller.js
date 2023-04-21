10ce4255218ca835a11beea9b1f6b73f2a2336c3
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-21 03:24:33 +0300

    checking if email exist

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,4 +26,8 @@ login = async (req, res) => {
 
     const { email, password } = req.body;
 
+    const user = await User.findOne({ email });
+      if (!user) 
+        return res.status(404).json({ message: "Invalid Credentials" });
+
 }
