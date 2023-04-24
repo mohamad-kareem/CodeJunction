6ec30fd6d2e4dc197912c232e434854f34a1b060
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-25 01:56:37 +0300

    implementing axios request to create a code folder

--- frontend/src/Components/CreateFolder/CreateFolder.jsx
+++ frontend/src/Components/CreateFolder/CreateFolder.jsx
@@ -1,38 +1,52 @@
-import React from 'react'
-import ButtonComponent from '../Button/ButtonComponent';
+import React ,{useState}from 'react'
+import { useParams } from "react-router-dom";
 import InputForm from '../InputForm/InputForm';
 import "./createfolder.css"
+import axios from "axios";
+
 const CreateFolder = ({HideFolder}) => {
+
+  const { roomId } = useParams(); 
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleCreateFolder = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.post(
+        "http://localhost:8000/createfolder/",
+        { title, description, roomId }, 
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log(response.data);
+      HideFolder();
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className='Create-code-folder-wrapper'>
-      
-       
       <div className='sign-in-up-container'>
-        
-             <div className="folder-inner-form">
-             <h2 className='folder-header'>Create Folder</h2>
-                  <InputForm
-                    label="Title"
-                    name="Title"
-                    type='text'
-                    color=' #0F0A36'
-                    
-                />
-                   <InputForm
-                    label="Description"
-                    name="Description"
-                    color=' #0F0A36'
-                    isTextarea={true}
-                    height={150}
-                />
-                <div className="folder-buttom">
-                <ButtonComponent color="yellow"size="15px" width='100px'>Create</ButtonComponent> 
-                <ButtonComponent color="yellow"size="15px" width='100px'marginLeft={20} onClick={HideFolder} >Close</ButtonComponent>
-                </div>
-                
-             </div>
-      </div>
-          
+        <div className="folder-inner-form">
+        <h2 className='folder-header'>Create Folder</h2>
+            <InputForm
+              label="Title"
+              name="Title"
+              type='text'
+              color=' #0F0A36'
+              onChange={(e) => setTitle(e.target.value)}
+          />
+              <InputForm
+              label="Description"
+              name="Description"
+              color=' #0F0A36'
+              isTextarea={true}
+              height={150}
+              onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+      </div>  
     </div>
   )
 }
