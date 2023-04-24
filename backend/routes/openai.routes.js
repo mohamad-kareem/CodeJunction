35ac6794e1ebf8e09a24661ba8018b11f1bff0f1
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-25 01:33:47 +0300

    adding advice openai route

--- backend/routes/openai.routes.js
+++ backend/routes/openai.routes.js
@@ -2,9 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 const { authMiddleware } = require("../midllewares/auth.middleware");
+const { analyzeCode,adviceCode} = require('../openai');
 
-const { analyzeCode} = require('../openai');
 router.post('/analyze',authMiddleware, analyzeCode);
 
+router.post('/advice',authMiddleware, adviceCode);
 
 module.exports = router;
