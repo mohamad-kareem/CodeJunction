1aaa7db56f0fb13f95049c6143673c35b53208ef
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-26 06:34:10 +0300

    fetching user info from data base

--- frontend/src/Components/PersonalInfo/PersonalInfo.jsx
+++ frontend/src/Components/PersonalInfo/PersonalInfo.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import emptyimage from "../../assets/empty-pic.jpg"
 import EditIcon from '@mui/icons-material/Edit';
 import "./personal.css"
+import axios from 'axios';
 const PersonalInfo = () => {
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/userInfo', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+        setUserInfo(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserInfo();
+  }, []);
   return (
     <div className="personal-info">
               <div className="inner-personal-info">
@@ -10,9 +28,9 @@ const PersonalInfo = () => {
                      <img src={emptyimage} className='small-pic' alt="pic" />
                   </div>
                   <div className="personal-text">
-                      <h2>mohamad karim</h2>
-                      <p>Full stack web developer</p>
-                      <h3>Computer and communication engineer.</h3>
+                      <h2 className='Username'></h2>
+                      <p className='Job'></p>
+                      <h3 className='Profession'></h3>
                   </div>
               </div>
               <div className="edit-icon-container">
