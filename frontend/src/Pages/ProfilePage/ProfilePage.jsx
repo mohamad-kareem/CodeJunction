6d6b7c0fdfd15ad25995f6c2068ff2ce5d34d33f
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-26 05:54:11 +0300

    updating profile axios request

--- frontend/src/Pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,18 +1,43 @@
 import React,{useState} from 'react'
 import SideBar from '../../Components/SideBar/SideBar'
 import "./profilepage.css"
-import {faEnvelope,faUserSecret,faMapMarker} from '@fortawesome/free-solid-svg-icons';
-
+import {faEnvelope,faUserSecret,faBriefcase, faUserTie} from '@fortawesome/free-solid-svg-icons';
 import InputForm from '../../Components/InputForm/InputForm';
 import ButtonComponent from '../../Components/Button/ButtonComponent';
 import emptyImage from "../../assets/empty-pic.jpg"
+import axios from 'axios';
 const ProfilePage = () => {
+
   const [image, setImage] = useState(emptyImage);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [job, setJob] = useState('');
+  const [profession, setProfession] = useState('');
 
   const handleImageChange = (event) => {
     const selectedImage = URL.createObjectURL(event.target.files[0]);
     setImage(selectedImage);
   };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const updatedProfileData = {};
+    axios
+      .put('http://127.0.0.1:8000/update', updatedProfileData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
   return (
     <div className='profile-wrapper'>
         <SideBar/>
@@ -38,18 +63,27 @@ const ProfilePage = () => {
                     name="username"
                     type='text'
                     icon={faUserSecret}
+                    value={username} setValue={setUsername}
                 />
                   <InputForm
                     label="email address"
                     name="email"
                     icon={faEnvelope}
+                    value={email} setValue={setEmail}
                 />
                 <InputForm
-                    label="location"
-                    name="location"
-                    icon={faMapMarker}
+                    label="Job"
+                    name="Job"
+                    icon={faBriefcase}
+                    value={job} setValue={setJob}
                 />
-                <ButtonComponent color="yellow"size="15px">Update Profile</ButtonComponent> 
+                <InputForm
+                    label="profession"
+                    name="profession"
+                    icon={faUserTie}
+                    value={profession} setValue={setProfession}
+                />
+                <ButtonComponent color="yellow"size="15px" onClick={handleSubmit}>Update Profile</ButtonComponent> 
             </div>
             </div>
           </div>
