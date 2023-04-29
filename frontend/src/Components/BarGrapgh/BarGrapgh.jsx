e5fd6a56cc22dab55fa0adf9c9fb6f4cce86a8dd
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-29 21:34:29 +0300

    testing bargrapgh

--- frontend/src/Components/BarGrapgh/BarGrapgh.jsx
+++ frontend/src/Components/BarGrapgh/BarGrapgh.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const BarGrapgh = () => {
   return (
-    <div>
-      
+    <div className='bargrapgh'>
+      <div className='bar-grapgh title'>AI Usage</div>
+
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="/cents" />
+          <YAxis dataKey="date"/>
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+
     </div>
   )
 }
--- frontend/src/Components/Chart/Chart.jsx
+++ frontend/src/Components/Chart/Chart.jsx
@@ -29,24 +29,31 @@ const Chart = () => {
         <div className="title">Last Time Coded</div>
         <ResponsiveContainer width="100%" aspect={4/1}>
 
-        <AreaChart width={730} height={250} data={data}
+          <AreaChart 
+            width={730} 
+            height={250}
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
 
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-          <defs>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
 
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-            </linearGradient>
-            
-          </defs>
-          <XAxis dataKey="month" />
-          <YAxis dataKey="count"/>
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area type="monotone" dataKey="count" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+            <XAxis dataKey="month" />
+            <YAxis dataKey="count"/>
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+             type="monotone"
+             dataKey="count" 
+             stroke="#8884d8" 
+             fillOpacity={1} 
+             fill="url(#colorUv)" />
 
-        </AreaChart>
+          </AreaChart>
 
         </ResponsiveContainer>
     </div>
