4b0270e1663affa2fe16d3bc2ca7802276e4825e
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-29 04:16:55 +0300

    adding animation to advice component

--- frontend/src/Components/Advice/Advice.jsx
+++ frontend/src/Components/Advice/Advice.jsx
@@ -1,35 +1,39 @@
 import React from 'react'
 import "./advice.css"
 import robot from "../../assets/robo_small.gif"
+import { motion } from 'framer-motion'
 const Advice = ({advice,HideAdvice}) => {
 
 const variants = {
   hidden: { 
-    opacity: 0,
-    y: "50%"
-  },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.5 }
-  },
-  exit: {
-    opacity: 0,
-    y: "50%",
-    transition: { duration: 0.5 }
-  }
+      opacity: 0,
+      y: "50%"
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 3 }
+    },
+    exit: {
+      opacity: 0,
+      y: "50%",
+      transition: { duration: 3 }
+    }
 }
 
   return (
-    <div className='ai-advice'>
+    <div className='ai-advice' onClick={HideAdvice}>
         <div className='sign-in-up-container'>
-        <div className="advice-inner-form">
-            <div className="advice-flex">
-            <img src={robot} className='robot' onClick={HideAdvice}/>
-            <br />{ advice} 
-            </div>
-       
-      </div>
+          < motion.div 
+            className="advice-inner-form" 
+            variants={variants}initial="hidden"
+            animate="visible"
+            exit="exit">
+              <div className="advice-flex">
+              <img src={robot} className='robot' />
+              <br />{ advice} 
+              </div>
+          </motion.div>
       </div>
     </div>
   )
