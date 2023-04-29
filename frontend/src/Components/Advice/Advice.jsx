81473478625b1a962a5634229a9d9534e3e7aa7a
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-29 04:00:18 +0300

    adding some variants to advice animation

--- frontend/src/Components/Advice/Advice.jsx
+++ frontend/src/Components/Advice/Advice.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import "./advice.css"
 import robot from "../../assets/robo_small.gif"
 const Advice = ({advice,HideAdvice}) => {
+
+const variants = {
+  hidden: { 
+    opacity: 0,
+    y: "50%"
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5 }
+  },
+  exit: {
+    opacity: 0,
+    y: "50%",
+    transition: { duration: 0.5 }
+  }
+}
+
   return (
     <div className='ai-advice'>
         <div className='sign-in-up-container'>
