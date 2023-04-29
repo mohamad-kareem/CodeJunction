e029f71bf149ff8f502f4cf80f77981f52dd2f34
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-04-30 02:52:07 +0300

    updating the daily value schema

--- backend/models/adminStatistics.js
+++ backend/models/adminStatistics.js
@@ -3,16 +3,15 @@ const mongoose = require("mongoose");
 const DailyValueSchema = new mongoose.Schema({
     date: {
         type: Date,
-        default: Date.now,
-        unique: true,
+        unique:true,
+        required: true,
     },
     value: {
         type: Number,
-        required: true,
         default: 0,
     },
 });
 
-const DailyValue = mongoose.model("WebsiteDailyValue", DailyValueSchema);
+const DailyValue = mongoose.model("DailyValue", DailyValueSchema);
 
 module.exports = DailyValue;
