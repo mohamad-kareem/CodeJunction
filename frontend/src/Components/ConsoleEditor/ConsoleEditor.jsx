78e2ce7acce9dc9c8e96d56a32866e5c7e58afab
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-01 02:56:59 +0300

    adding hide option on console

--- frontend/src/Components/ConsoleEditor/ConsoleEditor.jsx
+++ frontend/src/Components/ConsoleEditor/ConsoleEditor.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import "./consoleeditor.css"
-const ConsoleEditor = ({output}) => {
-    
-    return (
-        <div className='wrapper-console'>
-          <div className="output-tabs output-tabs-horizontal">
-            <div className="left-output">
-                <div className="output-tab">Terminal</div>
-                <div className="output-tab">AI</div>
-            </div>
-            <div className="right-output">
-                <div className="output-tab">Hide</div>
-            </div>
-          </div>
-          <div className="result">
-            <textarea className='output-result' value={output} ></textarea>
+import React, { useState } from 'react';
+import './consoleeditor.css';
+
+const ConsoleEditor = ({ output }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleHideClick = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <div className={`wrapper-console ${isVisible ? 'visible' : 'hidden'}`}>
+      <div className="output-tabs output-tabs-horizontal">
+        <div className="left-output">
+          <div className="output-tab">Terminal</div>
+          <div className="output-tab">AI</div>
+        </div>
+        <div className="right-output">
+          <div className="output-tab" onClick={handleHideClick}>
+            Hide
           </div>
         </div>
-      )
-    }
+      </div>
+      <div className="result">
+        <textarea className="output-result" value={output}></textarea>
+      </div>
+    </div>
+  );
+};
 
-export default ConsoleEditor
+export default ConsoleEditor;
