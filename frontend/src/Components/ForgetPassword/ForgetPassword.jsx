453238a99011bce6c7e61c69578dbc5385b73741
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-02 00:27:01 +0300

    adding navbar for forget password

--- frontend/src/Components/ForgetPassword/ForgetPassword.jsx
+++ frontend/src/Components/ForgetPassword/ForgetPassword.jsx
@@ -2,24 +2,27 @@ import React from 'react'
 
 const ForgetPassword = () => {
   return (
-    <div className='wrapper'>
-      <div className='image-container'>
-        <img src={loginImage} alt='registration' />
-      </div>
-      <div className='sign-in-up-container'>
-        <form className='inner-form'>
-            <h2 className='header'>Sign In</h2>
-              <InputForm
-                label="email address"
-                name="email"
-                value={email}
-                onChange={HandleEmailChange}
-                icon={faEnvelope}
-            />
-            <ButtonComponent color="yellow"size="15px" onClick={HandleSubmit}>Reset Password</ButtonComponent> 
-        </form>
-      </div>
-    </div>
+    <>
+        <Navbar/>
+        <div className='wrapper'>
+        <div className='image-container'>
+            <img src={loginImage} alt='registration' />
+        </div>
+        <div className='sign-in-up-container'>
+            <form className='inner-form'>
+                <h2 className='header'>Sign In</h2>
+                <InputForm
+                    label="email address"
+                    name="email"
+                    value={email}
+                    onChange={HandleEmailChange}
+                    icon={faEnvelope}
+                />
+                <ButtonComponent color="yellow"size="15px" onClick={HandleSubmit}>Reset Password</ButtonComponent> 
+            </form>
+        </div>
+        </div>
+    </>
   )
 }
 
