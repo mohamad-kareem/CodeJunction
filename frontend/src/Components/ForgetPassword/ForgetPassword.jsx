afb519c0df45d8ffd0c5ebc66653e3f60789d5f1
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-02 00:25:59 +0300

    adding input form for forget password page

--- frontend/src/Components/ForgetPassword/ForgetPassword.jsx
+++ frontend/src/Components/ForgetPassword/ForgetPassword.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const ForgetPassword = () => {
   return (
-    <div>
-      
+    <div className='wrapper'>
+      <div className='image-container'>
+        <img src={loginImage} alt='registration' />
+      </div>
+      <div className='sign-in-up-container'>
+        <form className='inner-form'>
+            <h2 className='header'>Sign In</h2>
+              <InputForm
+                label="email address"
+                name="email"
+                value={email}
+                onChange={HandleEmailChange}
+                icon={faEnvelope}
+            />
+            <ButtonComponent color="yellow"size="15px" onClick={HandleSubmit}>Reset Password</ButtonComponent> 
+        </form>
+      </div>
     </div>
   )
 }
