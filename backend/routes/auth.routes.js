440b3b30ea9921f318107cca852f88e5fcdfa6b1
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-02 02:09:34 +0300

    Backend:adding reset password route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const { login, register,forgetPasswordNotification,forgotpassword } = require("../controllers/auth.controller")
+const { login, register,forgetPasswordNotification,forgotpassword,resetpassword } = require("../controllers/auth.controller")
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password-notification",forgetPasswordNotification)
+router.post("/reset-password/:id/:token",resetpassword)
 
 router.get("/reset-password/:id/:token",forgotpassword)
 
