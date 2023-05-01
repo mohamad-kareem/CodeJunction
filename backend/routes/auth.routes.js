0f8f39ec7f82035985196cb03d8ecc7cc2378f4e
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-02 01:46:24 +0300

    Backend:adding forget password route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 
-const { login, register,forgetPasswordNotification } = require("../controllers/auth.controller")
+const { login, register,forgetPasswordNotification,forgotpassword } = require("../controllers/auth.controller")
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password-notification",forgetPasswordNotification)
+
+router.get("/reset-password/:id/:token",forgotpassword)
 
 module.exports = router;
