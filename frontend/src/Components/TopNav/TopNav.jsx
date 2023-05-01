9dfac40ab0f9171e949171881d865ea945432b91
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-01 04:41:27 +0300

    creating TopNav component

---
+++ frontend/src/Components/TopNav/TopNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./top.css"
+const TopNav = () => {
+  return (
+    <div className="top-section">
+        <div className="top-title">
+            <h1 className='top-name'>Edit profile</h1>
+        </div>
+  </div>
+  )
+}
+
+export default TopNav
