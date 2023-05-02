8f27e30571bd036360adede06bb7707eb80a32e4
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-02 07:25:03 +0300

    Frontend:adding useEffect and linking the data with piechart

--- frontend/src/Components/PieChart/PieChart.jsx
+++ frontend/src/Components/PieChart/PieChart.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {CircularProgressbar,buildStyles} from "react-circular-progressbar"
 import "react-circular-progressbar/dist/styles.css"
+import axios from 'axios'
 const PieChart = () => {
-
-    const getUsersCount = async () => {
-        try {
-            const response = await axios.get('http://localhost:8000/getAllUsersNumber', {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
-            },
-            });
-            return response.data.count;
-        }catch (error) {
-            console.log(error);
-        }
-    }; 
-
+    const [usersCount, setUsersCount] = useState(null);
+    useEffect(() => {
+        const getUsersCount = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/getAllUsersNumber', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+                });
+                setUsersCount(response.data.count);
+            }catch (error) {
+                console.log(error);
+            }
+        }; 
+        getUsersCount();
+    }, []);
   return (
     <div>
       <CircularProgressbar
-        value={50} 
-        text='40%' 
+        value={usersCount}
+        text={`${usersCount}%`}
         strokeWidth={3} 
         styles={buildStyles({
         pathColor: "yellow",
