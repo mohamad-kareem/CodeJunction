d4da4c383bff71d41e23a5a8e4441dc0dd4cd574
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-03 09:44:19 +0300

    Frontend:adding use effect to client color container

--- frontend/src/Components/Client/Client.jsx
+++ frontend/src/Components/Client/Client.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Avatar from 'react-avatar';
 
-const Client = ({username}) => {
-  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+const Client = ({ username }) => {
+  const [randomColor, setRandomColor] = useState('#000000');
+
+  useEffect(() => {
+    const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    setRandomColor(color);
+  }, []);
 
   return (
     <div className='client-container'>
@@ -12,4 +17,4 @@ const Client = ({username}) => {
   )
 }
 
-export default Client
+export default Client;
