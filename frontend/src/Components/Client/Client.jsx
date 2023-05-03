3a9bc49cca7f6e4056ea7b35c37645f800a07f66
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-03 08:49:18 +0300

    Frontend:generating random color each time client enters session

--- frontend/src/Components/Client/Client.jsx
+++ frontend/src/Components/Client/Client.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Avatar from 'react-avatar';
+
 const Client = ({username}) => {
-  
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+
   return (
     <div className='client-container'>
-      <Avatar color={Avatar.getRandomColor( ['yellow',"red"])} name={username} size={40} round="10px"/>
+      <Avatar color={randomColor} name={username} size={40} round="10px"/>
       <span className='username'>{username}</span>
     </div>
   )
