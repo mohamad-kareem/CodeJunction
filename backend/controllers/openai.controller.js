13c0fcf57f71e033d521789c0c275629dd03b052
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-04 07:19:33 +0300

    Backend:fixing the logic in analyzing code api

--- backend/controllers/openai.controller.js
+++ backend/controllers/openai.controller.js
@@ -12,37 +12,49 @@ const analyzeCode = async (req, res) => {
     try {
         const { code } = req.body;
         const prompt = `
-        Just give me a single score between 1 and 5 its hard to get 5 unless the code is very good,
-        Please assign a score between 0 and 4 points where 0 indicates poor quality and 4 indicates excellent quality depending on the following criteria:
-        important--> error:if any error in the code then the score is equal 0.
-        Syntax: the code must use correct syntax and formatting Is it easy to read and understand if not then a low score will be assigned.
-        Structure:the code must be organized in a logical and clear manner and the functions, loops, and conditionals used appropriately if not low score as well.
-        Functionality: the code must correctly perform its intended task and there must be clear from bugs or errors if not low score else high score
-        Readability: the code must be easy to read and comprehend and the variable names and comments descriptive and helpful if yes hight score else low score.
-        important--> Length:Is the code short or big if short (less than 4 words then score=1) else you decide.
-        the response must be Just the score without any more word for example score:1 or score:2 or score:3 or score:4 or score 5 !!
-        \
+
         here is the python code :
         ${code}
-        \
-        
+
+        Please assign a score between 0 and 4 points for the above python code where 0 indicates poor quality and 4 indicates excellent quality 
+        depending on the following criteria:
+           
+            error:if any error in the code then the score is equal 0.
+
+            Syntax: the code must use correct syntax and formatting Is it easy to read and understand if not then a low score will be assigned.
+
+            Structure:the code must be organized in a logical and clear manner and the functions, loops, and conditionals used appropriately if not low score as well.
+            
+            Functionality: the code must correctly perform its intended task and there must be clear from bugs or errors if not low score else high score
+           
+            Readability: the code must be easy to read and comprehend and the variable names and comments descriptive and helpful if yes hight score else low score.
+            
+          Please get attention if the  Length of the code is short  (less than 4 words then score will be one) else you decide.
+          
+          Finally the response must be Just the score without any more word for example score:1 or score:2 or score:3 or score:4!!
         `;
 
         const response = await openai.createCompletion({
         model: 'text-davinci-003',
         prompt: prompt,
-        max_tokens: 2000,
-        temperature: 0.1,
+        max_tokens: 3000,
+        temperature: 1,
         });
 
         const answer = response.data.choices[0].text.trim();
-
-        const userId = req.user._id;
-        const user = await User.findById(userId);
-        user.points += 1;
-        await user.save();
-
-        res.json({ answer });
+        console.log(answer)
+        const score = answer.match(/\d+/);
+        if(score){
+          const numericScore = parseInt(score[0]);
+          const userId = req.user._id;
+          const user = await User.findById(userId);
+          user.points += numericScore;
+          await user.save();
+          res.json({ answer :numericScore});
+        }else {
+          res.status(500).json({ message: 'Unable to extract score from OpenAI response' });
+        }
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server error' });
