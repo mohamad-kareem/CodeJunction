6d3ff47d5cf6295283e65732d81ad1f1f15d36fa
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-06 01:28:50 +0300

    Frontend:mapping on footer labels and links

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -4,16 +4,25 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import "./footer.css"
 import CopyRight from '../CopyRight/CopyRight';
+
+const footerLinks = [
+  { label: 'Mobile app', url: 'https://CodeJunction.com/mobile-app' },
+  { label: 'Community', url: 'https://CodeJunction/community' },
+  { label: 'Company', url: 'https://CodeJunction/company' },
+  { label: 'Help desk', url: 'https://CodeJunction/help-desk' },
+  { label: 'Blog', url: 'https://CodeJunction/blog' },
+  { label: 'Resources', url: 'https://CodeJunction/resources' },
+];
+
 const Footer = () => {
   return (
     <div className="footer">
     <div className="info">
-    <a href="https://CodeJunction.com/mobile-app"><p className='footer-details'>Mobile app</p></a>
-    <a href="https://CodeJunction/community"><p className='footer-details'>Community</p></a>
-    <a href="https://CodeJunction/company"><p className='footer-details'>Company</p></a>
-    <a href="https://CodeJunction/help-desk"><p className='footer-details'>Help desk</p></a>
-    <a href="https://CodeJunction/blog"><p className='footer-details'>Blog</p></a>
-    <a href="https://CodeJunction/resources"><p className='footer-details'>Resources</p></a>
+      {footerLinks.map((link,index)=>(
+        <a href={link.url} key={index}>
+          <p className='footer-details'>{link.label}</p>
+        </a>
+      ))}
     </div>
     <div className="connect">
         <div className="icons-container">
