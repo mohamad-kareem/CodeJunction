10df64bf952d2631da78a92d954aaefc139781c8
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-06 01:32:06 +0300

    Frontend:using useNavigate hook instead of window.location

--- frontend/src/Components/LandingIntro/LandingIntro.jsx
+++ frontend/src/Components/LandingIntro/LandingIntro.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import "./landingintro.css"
 import ButtonComponent from '../Button/ButtonComponent'
 
-const LandingIntro = () => {
-    const handleClick = () => {
-        window.location.href = "/register";
-      }
-    
+
+const LandingIntro = () => {    
+  const navigate = useNavigate()
+  
   return (
     <div className='intro-container'>
       <div className='intro-content'>
               <h1>Unlock your coding<br/>potential with collaboration</h1>
               <div className='btn-intro'> 
-                <ButtonComponent width='130px'size={"15px"} onClick={handleClick}>Get Started</ButtonComponent>
+                <ButtonComponent width='130px'size={"15px"} onClick={() => navigate("/register")}>Get Started</ButtonComponent>
               </div>
       </div>
     </div>
