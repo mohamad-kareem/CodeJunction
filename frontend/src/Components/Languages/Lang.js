ca9f7e855874a7e5efd3737498e42b41f1591bde
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-10 04:27:57 +0300

    Frontend:implementing navbar translation object

---
+++ frontend/src/Components/Languages/Lang.js
@@ -0,0 +1,13 @@
+const Nav= {
+    en:{
+       compiler:"compile",
+       Dashboard: "Dashboard",
+       Logout:"LogOut"
+    },
+    ch:{
+        compiler:"编译器",
+        Dashboard: "仪表板",
+        Logout:"退出登录"
+    }
+}
+export default Nav;
